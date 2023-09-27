59bb81ee71c0112e5b016c3074dac30d1544a3d5
Omer <uruco339@gmail.com>
2023-09-27 06:10:06 +0200

    Layout1

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Vans from './pages/vans/Vans';
-import Header from './pages/header/Header';
-import Footer from './pages/footer/Footer';
 import VanDetails from './pages/vans/VanDetails';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/vans' element={<Vans />} />
-        <Route path='/vans/:id' element={<VanDetails />} />
+        <Route  element={<Layout/>}>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/vans' element={<Vans />} />
+          <Route path='/vans/:id' element={<VanDetails />} />
+        </Route>
       </Routes>
-
-      <Footer />
     </BrowserRouter>
   );
 }
---
+++ src/components/Layout.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from './header/Header';
+import Footer from './footer/Footer';
+
+/**
+ * 
+ <Outlet> Component: 
+ In this code, you are using the <Outlet> component inside the Layout component. The <Outlet> component is a special component provided by React Router. It acts as a placeholder for the child routes defined within a parent route. In this case, child routes like <Route path='/' element={<Home />} /> and <Route path='/about' element={<About />} /> will be rendered inside the <Outlet> in the Layout component. This allows you to nest route content within your layout.
+ */
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+
+      <Outlet />
+
+      <Footer/>
+    </div>
+  );
+};
+
+export default Layout;
